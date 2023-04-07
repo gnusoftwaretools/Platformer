b9622541c1e3ad23250a68e41722fd1360f15c7c
io <zubz0@protonmail.com>
2023-04-07 07:02:59 -0700

    Init

---
+++ Assets/_Scripts/Agent.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public PlayerInput playerInput;
+    public AgentAnimation animationManager;
+    public AgentRenderer agentRenderer;
+    private void Awake()
+    {
+        playerInput = GetComponentInParent<PlayerInput>();
+        rb2d = GetComponent<Rigidbody2D>();
+        animationManager = GetComponentInChildren<AgentAnimation>();
+        agentRenderer = GetComponentInChildren<AgentRenderer>();
+    }
+
+    private void Start()
+    {
+        playerInput.OnMovement += HandleMovement;
+        playerInput.OnMovement += agentRenderer.FaceDirection;
+    }
+
+    private void HandleMovement(Vector2 input)
+    {
+        if (Mathf.Abs(input.x) > 0)
+        {
+            if (Mathf.Abs(rb2d.velocity.x) < 0.01f)
+            {
+                animationManager.PlayAnimation(AnimationType.run);
+            }
+            rb2d.velocity = new Vector2(input.x * 5, rb2d.velocity.y);
+        }
+        else
+        {
+            if (Mathf.Abs(rb2d.velocity.x) > 0)
+            {
+                animationManager.PlayAnimation(AnimationType.idle);
+            }
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        }
+    }
+}
